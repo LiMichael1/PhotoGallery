04d3136550971a5fb6b21b686e0230a36804d875
rpura951 <rpura951@gmail.com>
2020-02-19 16:24:02 -0800

    Updated to make fields mandatory

--- ralph_add.php
+++ ralph_add.php
@@ -9,19 +9,19 @@
 			<table style="border: 70px;">
 			<tr>
 				<td style="width: 100px; text-align: left;">Photo Name:</td>
-				<td><input type="text" name="photoname" size="20" maxlength="20" /></td>
+				<td><input type="text" name="photoname" required size="20" maxlength="20" /></td>
 			</tr>
 			<tr>
 				<td style="width: 100px; text-align: left;">Date Taken:</td>
-				<td><input type="date" name="datetaken" size="20" maxlength="20" /></td>
+				<td><input type="date" name="datetaken" required size="20" maxlength="20" /></td>
 			</tr>
 			<tr>
 				<td style="width: 100px; text-align: left;">Photographer:</td>
-				<td><input type="text" name="photographer" size="20" maxlength="20" /></td>
+				<td><input type="text" name="photographer" required size="20" maxlength="20" /></td>
 			</tr>
 			<tr>
 				<td style="width: 100px; text-align: left;">Location:</td>
-				<td><input type="text" name="location" size="20" maxlength="20" /></td>
+				<td><input type="text" name="location" requiredsize="20" maxlength="20" /></td>
 			</tr>
 			<tr>
 				<td></td>
@@ -33,7 +33,7 @@
 			</tr>
 			<tr>
 				<td></td>
-				<td style="text-align: center;"><button type="submit" formaction="ralph_view.php" style="background-color:blue; color:white; width:160px" name="submit1"/>View Photos</button?</td>
+				<td style="text-align: center;"><button type="submit" formaction="ralph_view.php" formnovalidate style="background-color:blue; color:white; width:160px" name="submit1"/>View Photos</button?</td>
 			</tr>
 			</table>
 		</form>
@@ -74,7 +74,7 @@
 					file_put_contents("uploads/list.txt", $_POST['photographer'], FILE_APPEND);
 					file_put_contents("uploads/list.txt", "\n", FILE_APPEND);
 					file_put_contents("uploads/list.txt", $_POST['location'], FILE_APPEND);
-					file_put_contents("uploads/list.txt", "\n\n", FILE_APPEND);
+					file_put_contents("uploads/list.txt", FILE_APPEND);
 					echo "The file ".basename($_FILES["SelectFile"]["name"])." has been uploaded.";
 					#fclose($fp);
 				}
